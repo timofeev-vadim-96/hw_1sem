89a84bc60e7adb22590c4b141c5642cd6fadeb83
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2022-11-27 16:18:36 +0300

    clear master

--- Program.cs
+++ Program.cs
@@ -34,24 +34,3 @@
 //     negativeNumber++;  // negativeNumber = negativeNumber + 1
 //     // negativeNumber+=1
 // }
-
-Console.WriteLine("This programm compare two numbers and write the largest number of them");
-Console.WriteLine("Please, enter the first number");
-int FirstNumb = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Please, enter the second number");
-int SecondNumb = Convert.ToInt32(Console.ReadLine());
-if (FirstNumb == SecondNumb)
-{
-    Console.WriteLine("The numbers are equal");
-}
-else
-{
-    if (FirstNumb > SecondNumb)
-    {
-        Console.WriteLine(FirstNumb + " is the largest number");
-    }
-    else 
-    {
-        Console.WriteLine(SecondNumb + " is the largest number");
-    }
-}
