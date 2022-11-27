d93e9dbbca3b7ab2200b1417722de6b5996dfdb6
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2022-11-27 16:14:07 +0300

    added git ignore

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿// // // Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+// // // Например:
+// // // 4 -> 16 
+// // // -3 -> 9 
+// // // -7 -> 49
+
+// // Console.WriteLine("Введите число");
+// // int number = Convert.ToInt32(Console.ReadLine());
+// // int result = number*number;
+// // Console.WriteLine(result);
+// Console.Write("Введите первое число: "); // Текст-подсказка, которая помогает получить число
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: "); // Текст-подсказка, которая помогает получить число
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+// if (secondNumber * secondNumber == firstNumber)
+// {
+//     System.Console.WriteLine("Первое число является квадратом второго");
+// }
+// else System.Console.WriteLine("Первое число НЕ является квадратом второго");
+
+// 5. Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+// int positiveNumber = Convert.ToInt32(Console.ReadLine()); // N
+
+// int negativeNumber = positiveNumber * (-1); // -5 = 5 * (-1), -N
+
+// while (negativeNumber <= positiveNumber) // пока условие истинно
+// {
+//     Console.Write(negativeNumber + " ");
+//     negativeNumber++;  // negativeNumber = negativeNumber + 1
+//     // negativeNumber+=1
+// }
+
+Console.WriteLine("This programm compare two numbers and write the largest number of them");
+Console.WriteLine("Please, enter the first number");
+int FirstNumb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please, enter the second number");
+int SecondNumb = Convert.ToInt32(Console.ReadLine());
+if (FirstNumb == SecondNumb)
+{
+    Console.WriteLine("The numbers are equal");
+}
+else
+{
+    if (FirstNumb > SecondNumb)
+    {
+        Console.WriteLine(FirstNumb + " is the largest number");
+    }
+    else 
+    {
+        Console.WriteLine(SecondNumb + " is the largest number");
+    }
+}
